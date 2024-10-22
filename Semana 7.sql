22d7b0985c81b15ae2f144f7cf7a402ac4a4fd60
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-10-21 19:04:47 -0600

    Update Semana 7.sql

--- Semana 7.sql
+++ Semana 7.sql
@@ -7,4 +7,12 @@ Drop Database BancoBD
 BACKUP DATABASE BancoBD TO DISK = 'C:\Backup\BancoBD.bak'
 
 
---Hola
+--indicar BD 
+Use BancoBD
+
+-- Crear una tabla
+Create Table Cliente (
+ID_Cliente Int Primary Key identity(1,1),
+Nombre Varchar(100) Not Null,
+Email Varchar (250) Unique Not Null
+)
