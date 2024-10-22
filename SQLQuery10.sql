7c34fad9914148bbe58fe66620ad4a4373947aa4
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-10-21 18:39:10 -0600

    bsalmeron

---
+++ SQLQuery10.sql
@@ -0,0 +1,7 @@
+--DDL 
+--Creación de una base de datos
+Create Database BancoBD
+--Eliminar base de datos 
+Drop Database BancoBD
+-- Backup e
+BACKUP DATABASE BancoBD TO DISK = 'C:\Backup\BancoBD.bak'
