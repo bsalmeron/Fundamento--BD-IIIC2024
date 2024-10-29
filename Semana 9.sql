68c9acf0cd2fef837561ce696947dc66d498dc54
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-10-28 18:41:40 -0600

    Update Semana 9.sql

--- Semana 9.sql
+++ Semana 9.sql
@@ -4,4 +4,38 @@
 -- Venta ID, ID_Cliente, ID_Producto, Fecha_Compra Default del 
 --dia y hora de insercciòn
 
---Nombre Datetime Default GETDATE()
+--Fecha_Compra Datetime Default GETDATE()
+
+ CREATE Database SuperMercadoBD
+
+ Use SuperMercadoBD
+
+
+ Create table Cliente (
+ ID Int Primary Key Identity(1,1), 
+ Nombre Varchar (50) Not null, 
+ Correo varchar(100) Not null 
+ )
+
+ Create table Producto(
+ ID Int Primary Key Identity(1,1), 
+ Nombre Varchar (50) Not null
+ )
+
+
+ Create table Venta (
+ ID Int Primary key identity(1,1), 
+ ID_Cliente Int Not null, 
+ ID_Producto Int Not null, 
+ Fecha_Compra Datetime Default GETDATE(), 
+ Foreign Key (ID_Cliente) References Cliente(ID), 
+ Foreign Key (ID_Producto) References Producto(ID) 
+ )
+
+ --CRUD  
+
+ --Crear o insertar 
+ Insert Into Cliente (Nombre,Correo)
+ Values('alonso','alonso@ucenfotec.ac.cr')
+
+ Select * from Cliente
