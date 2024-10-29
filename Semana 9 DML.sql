491728e0c0646653fd4b1411e93be5e2e3bde7e0
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-10-28 20:40:00 -0600

    Create Semana 9 DML.sql

---
+++ Semana 9 DML.sql
@@ -0,0 +1,19 @@
+
+Use Northwind
+--DML
+
+--Distinct
+Select Distinct  CustomerID, ShipName From Orders
+
+--Where 
+
+Select ProductName, CategoryID, UnitsInStock from Products
+Where CategoryID = 1
+order by UnitsInStock Desc
+
+-- Seleccionar todos los empleados, donde el Pais sea USA 
+-- y ordenarlo de forma asc por su fecha de cumpleaños
+-- Nombre, pais, fecha cumple, TitleOfCourtesy
+ 
+
+
