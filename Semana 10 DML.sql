ebf0109411c25f7237bc9af3744c29e24186888c
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-11-04 19:27:03 -0600

    Update Semana 10 DML.sql

--- Semana 10 DML.sql
+++ Semana 10 DML.sql
@@ -36,3 +36,29 @@ Inner Join Customers
 on Orders.CustomerID = Customers.CustomerID
 where Orders.ShipRegion is not null
 
+--Agregacion 
+
+
+Select  P.ProductName,
+SUM( OD.UnitPrice*OD.Quantity) 'precio Total' 
+from [Order Details] OD Inner Join Products P
+on OD.ProductID=P.ProductID
+Group by P.ProductName   
+
+
+
+/*
+Mostrar la cantidad de Ordenes, por nombre de cliente y nombre de producto comprado
+
+
+*/
+
+Select C.ContactName, p.productName, COUNT(O.OrderID)'cantidadOrdenes' from 
+Customers C Inner join 
+Orders O on C.CustomerID = O.CustomerID
+Inner Join [Order Details] OD 
+on O.OrderID = OD.OrderID
+Inner Join Products P 
+on OD.ProductID=P.ProductID
+Group by C.ContactName, p.productName
+Order by  C.ContactName asc , COUNT(O.OrderID) desc 
