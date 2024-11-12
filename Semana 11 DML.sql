1b434492e6c58b546c729f7a2cb856cc91c2d9e5
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-11-11 19:22:25 -0600

    Update Semana 11 DML.sql

--- Semana 11 DML.sql
+++ Semana 11 DML.sql
@@ -33,12 +33,34 @@ order by Inicio desc
 Select  empleados.nombre, COUNT(ProyectosGeneral.empleado_id) 'CantidadProyectos'  from
 (Select  empleado_id, fecha_inicio, NULL fecha_fin 
 from proyectos
-UNION  ALL
+UNION ALL   
 Select empleado_id, fecha_inicio, fecha_fin 
 from proyectos_historico) As ProyectosGeneral
 inner join empleados 
 on ProyectosGeneral.empleado_id = empleados.id
 Group by empleados.nombre
 
+--Subconsultas 
 
+Select * from empleados
+where salario > (select AVG(salario) from empleados)
+
+ Select AVG(salario) from 
+ (Select salario from empleados where departamento_id=1) AS salarios
+
+
+Select nombre from empleados 
+where departamento_id IN (Select id from departamentos
+where nombre='Ventas')
+
+select e.nombre from empleados e
+INNER JOIN departamentos d
+on e.departamento_id= d.id
+where d.nombre='Ventas'
+
+--Exists
+
+Select * from empleados
+where exists (select 1 from departamentos where 
+id= empleados.departamento_id)
 
