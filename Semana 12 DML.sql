3e13f454ce2557c869f071cd43d458b13805e6f9
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2024-11-18 18:45:33 -0600

    Update Semana 12 DML.sql

---
+++ Semana 12 DML.sql
@@ -0,0 +1,14 @@
+/*
+1- Procedimiento almacenado
+    a-Reglas de negocio complejas 
+    b-CRUD 
+    c-Un Componete (Usiario, Aplicaciones, Api Etc)
+    necesita conusmir nuestra base de datos
+2- Vistas
+    a- Solo funciona con select 
+	b- Estructura defina
+3-  Funciones 
+    Escalares ** 
+    a- Calculos estandar 
+    Tabla
+*/
